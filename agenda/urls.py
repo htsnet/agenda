0ea139a6c0c720ec0b233bf0ab800738c75ff5a6
Hamilton Tenorio da Silva <hamilton@hts-net.com.br>
2020-02-29 16:41:40 -0300

    Incluída a rotina de login/logout de usuários

--- agenda/urls.py
+++ agenda/urls.py
@@ -23,5 +23,8 @@ urlpatterns = [
     path('evento/<titulo_evento>', views.evento),
     path('agenda/', views.lista_eventos),
     # path('', views.index),
-    path('', RedirectView.as_view(url='/agenda/'))
+    path('', RedirectView.as_view(url='/agenda/')),
+    path('login/', views.login_user),
+    path('login/submit', views.submit_login),
+    path('logout/', views.logout_user),
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, HttpResponse, redirect
-
-# Create your views here.
 from core.models import Evento
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
 
 
 def evento(request, titulo_evento):
@@ -12,14 +14,34 @@ def evento(request, titulo_evento):
         texto = '<h1>Evento não localizado! Tente escrever novamente.<h1>'
     return HttpResponse(texto)
 
-
+@login_required(login_url='/login/')
 def lista_eventos(request):
     usuario = request.user
-    ##evento = Evento.objects.filter(usuario=usuario)
-    evento = Evento.objects.all()
+    evento = Evento.objects.filter(usuario=usuario)
+    #evento = Evento.objects.all()
     dados = {'eventos': evento}
     return render(request, 'agenda.html', dados)
 
 
 # def index(request):
 #     return redirect('/agenda/')
+
+
+def login_user(request):
+    return render(request, 'login.html')
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')
+
+def submit_login(request):
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        usuario = authenticate(username=username, password=password)
+        if usuario is not None:
+            login(request, usuario)
+            return redirect('/')
+        else:
+            messages.error(request, "Usuário ou senha inválida")
+    return redirect('/')
