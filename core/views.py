fcc3aa91c0e2d3cb0a18322a97dd7fca18896fd4
Hamilton Tenorio da Silva <hamilton@hts-net.com.br>
2020-02-29 19:51:32 -0300

    Inserida função de alteração e exclusão de evento

--- core/views.py
+++ core/views.py
@@ -5,7 +5,6 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 
-
 def evento(request, titulo_evento):
     try:
         registro = Evento.objects.get(titulo=titulo_evento)
@@ -52,18 +51,48 @@ def submit_login(request):
 
 @login_required(login_url='/login/')
 def evento(request):
-    return render(request, 'evento.html')
+    id_evento = request.GET.get('id')
+    dados = {}
+    if id_evento:
+        dados['evento'] = Evento.objects.get(id=id_evento)
+    return render(request, 'evento.html', dados)
 
 
 @login_required(login_url='/login/')
 def submit_evento(request):
     if request.POST:
         titulo = request.POST.get('titulo')
+        local = request.POST.get('local')
         data_evento = request.POST.get('data_evento')
         descricao = request.POST.get('descricao')
         usuario = request.user
-        Evento.objects.create(titulo=titulo,
+        id_evento = request.POST.get('id_evento')
+        if id_evento:
+            evento = Evento.objects.get(id=id_evento)
+            if evento.usuario == usuario:
+                evento.titulo = titulo
+                evento.descricao = descricao
+                evento.data_evento = data_evento
+                evento.local = local
+                evento.save()
+            # outra opção sem a verificação do id
+            # Evento.objects.filter(id=id_evento).update(titulo=titulo,
+            #                                            data_evento=data_evento,
+            #                                            descricao=descricao,
+            #                                            local=local)
+        else:
+            Evento.objects.create(titulo=titulo,
+                              local=local,
                               data_evento=data_evento,
                               descricao=descricao,
                               usuario=usuario)
+    return redirect('/')
+
+
+@login_required(login_url='/login/')
+def delete_evento(request, id_evento):
+    usuario = request.user
+    evento = Evento.objects.get(id=id_evento)
+    if usuario == evento.usuario:
+        evento.delete()
     return redirect('/')
