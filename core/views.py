8421672a1f04da80a707cca31c27d608c6397a1a
Hamilton Tenorio da Silva <hamilton@hts-net.com.br>
2020-02-29 18:27:26 -0300

    Inclusão de tela de inserção de dados de evento

--- core/views.py
+++ core/views.py
@@ -14,6 +14,7 @@ def evento(request, titulo_evento):
         texto = '<h1>Evento não localizado! Tente escrever novamente.<h1>'
     return HttpResponse(texto)
 
+
 @login_required(login_url='/login/')
 def lista_eventos(request):
     usuario = request.user
@@ -30,9 +31,11 @@ def lista_eventos(request):
 def login_user(request):
     return render(request, 'login.html')
 
+
 def logout_user(request):
     logout(request)
     return redirect('/')
+
 
 def submit_login(request):
     if request.POST:
@@ -44,4 +47,23 @@ def submit_login(request):
             return redirect('/')
         else:
             messages.error(request, "Usuário ou senha inválida")
+    return redirect('/')
+
+
+@login_required(login_url='/login/')
+def evento(request):
+    return render(request, 'evento.html')
+
+
+@login_required(login_url='/login/')
+def submit_evento(request):
+    if request.POST:
+        titulo = request.POST.get('titulo')
+        data_evento = request.POST.get('data_evento')
+        descricao = request.POST.get('descricao')
+        usuario = request.user
+        Evento.objects.create(titulo=titulo,
+                              data_evento=data_evento,
+                              descricao=descricao,
+                              usuario=usuario)
     return redirect('/')
